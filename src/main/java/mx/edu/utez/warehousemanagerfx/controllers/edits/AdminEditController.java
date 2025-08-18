2c75f1d64d89a1857c4996d10b79505bfb06a870
Haziel <20243ds097@utez.edu.mx>
2025-08-17 23:04:31 -0600

    ui(Admin): minor changes and fixes

--- src/main/java/mx/edu/utez/warehousemanagerfx/controllers/edits/AdminEditController.java
+++ src/main/java/mx/edu/utez/warehousemanagerfx/controllers/edits/AdminEditController.java
@@ -1,4 +1,30 @@
 package mx.edu.utez.warehousemanagerfx.controllers.edits;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import mx.edu.utez.warehousemanagerfx.utils.routes.FXMLRoutes;
+
+import java.util.Objects;
+
 public class AdminEditController {
+    @FXML
+    private void goHomeSA(ActionEvent event) {
+        try {
+            Parent loginWindow = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(FXMLRoutes.SUPERADMIN)));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene escena = new Scene(loginWindow);
+            stage.setScene(escena);
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error! Could not return to the admin window.");
+        }
+    }
 }
